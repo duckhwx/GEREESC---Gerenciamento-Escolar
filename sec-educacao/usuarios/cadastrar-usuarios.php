c987a756b17916d21dafdd89856819b6a7b8eab7
Thiaguinho-Mendes <jailsonthiagodm@gmail.com>
2019-08-21 17:28:08 -0300

    Adicinais para escola

---
+++ sec-educacao/usuarios/cadastrar-usuarios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Escolas</title>
+</head>
+<body>
+    <!--NAV BAR SEM ESTILO-->
+                    <a href="">Escola</a>
+
+                    <a href="">Usuários</a>     
+
+                    <a href="">Produtos</a> 
+
+                    <a href="">Cardápio</a> 
+
+                    <img src="">
+    
+    <!-- DESCONSIDERAR OS <BRS> TESTE -->
+    <br><br><br><br>
+
+    <a href="">Nutricionista</a>
+
+    <a href="">Secretário de Escola</a>
+
+
+</body>
+</html>
