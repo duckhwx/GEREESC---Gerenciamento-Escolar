90459c66baaa5d951f4ea243d30d22407e4785f0
SrDrougas <duckerson.henrique.carboni@gmail.com>
2019-08-27 01:41:52 -0300

    Alterando o index, retirando algumas tags e a sua folha de estilo

--- index.php
+++ index.php
@@ -1,39 +1,15 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="Estilo/styleLogin.css"/>
     <meta charset="UTF-8">
-    <title>GEREESC - Sistema de Gerenciamento de Estoque Escolar</title>
+    <title>Inicio</title>
 </head>
 <body>
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-2">
-                    /
-                </div>
-                <div class="col-8">
-                    <h1 id="Titulo">GEREESC <br> Sistema de Gerenciamento de Estoque</h1>
-                </div>
-                <div class="col-2">
-                    
-                </div>
-            </div>
-        </div>
-        </header>
-        <section>
-            <div id="Login">
-                    <div id="avatar"></div>
-                    <h4>Usuário</h4>
-                    <input type="texto" name="usuario">
-                    <br><br>
-                    <h4>Senha</h4>
-                    <input type="texto" name="senha">
-                    <br><br>
-                    <button type="submit" name="login">Logar</button>
-
-            </div>
-        </section>
+    <h1>GEREESC</h1>
+    
+    <form method="post" action="login/verificacao.php">
+        Usuário <input type="text" required maxlength="64" name="login">
+        Senha <input type="password" required maxlength="64" name="senha">
+        <input type="submit" value="Logar">
+    </form>
 </body>
 </html>
