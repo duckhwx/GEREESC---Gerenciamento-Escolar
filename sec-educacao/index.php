b9c5084c9e091db612dcef38d0630ed263c9dadc
Thiaguinho-Mendes <jailsonthiagodm@gmail.com>
2019-08-28 16:23:09 -0300

    adicionado tela incial

--- sec-educacao/index.php
+++ sec-educacao/index.php
@@ -3,7 +3,7 @@ require_once("../funcoes-de-cabecalho.php");
 //cabecalhoSecEdu("Secretário Da Educação", "escola", "usuarios", "produto", "cardapio");
 ?>
 
-<!DOCTYPE html> 
+<!DOCTYPE html>
     <html>
     <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
