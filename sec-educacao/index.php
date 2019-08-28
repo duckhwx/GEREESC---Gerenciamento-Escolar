fe34ada9b27aea2b5a422adeae598af95cafd5ce
Thiaguinho-Mendes <jailsonthiagodm@gmail.com>
2019-08-28 16:21:05 -0300

    Adicionando a pagina inicial do Sec. Educação

---
+++ sec-educacao/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once("../funcoes-de-cabecalho.php");
+//cabecalhoSecEdu("Secretário Da Educação", "escola", "usuarios", "produto", "cardapio");
+?>
+
+<!DOCTYPE html> 
+    <html>
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../estilo/styleCadastro.css"/>
+    <meta charset='UTF-8'>
+    <title>Inicio</title>
+    </head>
+    <body>
+        <header>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col'>
+                        <a href='escola/'>Escolas</a>
+                    </div>
+                    <div class='col'>
+                        <a href='../sec-educacao/usuarios/'>Usuários</a>
+                    </div>
+                    <div class='col'>
+                        <a href='../produto'>Produtos</a>
+                    </div>
+                    <div class='col'>
+                        <a href='../cardapio'>Cardápio</a>
+                    </div>
+                </div>
+            </div>
+        </header>   
+        <section>
+            <div id='outrascoisas'>
+                <!--AQUI APARECERAR ADICIONAIS FUTUROS -->
+            </div>
+        </section>
+
+
+<?php 
+rodape();
