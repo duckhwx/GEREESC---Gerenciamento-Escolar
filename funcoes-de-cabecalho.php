9d75adc7cef55937ce6dfecc2fc88364fa5f44de
Gustavo Kimura <gustavokimuraf@gmail.com>
2019-09-04 16:53:07 -0300

    Adição de estilo, bootsrap...

--- funcoes-de-cabecalho.php
+++ funcoes-de-cabecalho.php
@@ -11,7 +11,40 @@ function cabecalhoSecEdu($title = NULL, $linkEscola = NULL, $linkUsuarios = NULL
     . "<a href='$linkEscola'>Escolas</a>"
     . "<a href='$linkUsuarios'>Usuários</a>"
     . "<a href='$linkProduto'>Produtos</a>"
-    . "<a href='$linkCardapio'>Cardápio</a>";
+    . "<a href='$linkCardapio'>Cardápio</a>"
+    . "<br><br>";
+}
+
+function cabecalhoSecEsc($linkBootstrap = null, $title = null,$linkAluno = null,$linkEscola = null, $linkEstoque = null, $linkCardapio = null){
+    echo "<!DOCTYPE html>"
+    . "<html>"
+    . "<head>"
+    . "<meta charset='UTF-8'>"
+    . "<link href='$linkBootstrap' rel='stylesheet'>"
+    . "<title>$title</title>"
+    . "</head>"
+    . "<body>"
+    . "<a href='$linkAluno'>Alunos</a>"
+    . "<a href='$linkEscola'>Escolas</a>"
+    . "<a href='$linkEstoque'>Estoque</a>"
+    . "<a href='$linkCardapio'>Cardápio</a>"
+    . "<br><br>";
+}
+
+function cabecalhoNutricionista($linkBootstrap = null, $title = null,$linkEscola = null, $linkRelatorio = null, $linkProduto = null, $linkCardapio = null){
+    echo "<!DOCTYPE html>"
+    . "<html>"
+    . "<head>"
+    . "<meta charset='UTF-8'>"
+    . "<link href='$linkBootstrap' rel='stylesheet'>"
+    . "<title>$title</title>"
+    . "</head>"
+    . "<body>"
+    . "<a href='$linkEscola'>Escolas</a>"
+    . "<a href='$linkRelatorio'>Relatório</a>"
+    . "<a href='$linkProduto'>Produto</a>"
+    . "<a href='$linkCardapio'>Cardápio</a>"
+    . "<br><br>";
 }
 
 function rodape(){
---
+++ nutricionista/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once ('../funcoes-de-cabecalho.php');
+
+    cabecalhoNutricionista('../estilo/bootstrap/css/bootstrap-grid.php','Inicio','escola/index.php','relatorio/index.php','produto/index.php','cardapio/index.php');
+?>
+
+
+<?php
+
+    rodape();
