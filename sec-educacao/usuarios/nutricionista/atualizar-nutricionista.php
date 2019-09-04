80287a08b0b7d58fdb2a55641776197812e1d048
José Pedro Simão <josepedrosimao@gmail.com>
2019-09-04 15:52:35 -0300

    adiciona nutricionista

---
+++ sec-educacao/usuarios/nutricionista/atualizar-nutricionista.php
@@ -0,0 +1,26 @@
+<?php
+require_once '../../../funcoes-de-cabecalho.php';
+
+cabecalhoSecEdu("Atualizar Nutricionista", "../../escola", "../index.php", "../../produto", "../../cardapio");
+?>
+
+<form method="post" action="validar-nutricionista.php?acao=atualizar">
+    Nome <input type="text" required maxlength="64" name="nome"><br>
+    CPF <input type="text" required maxlength="11" name="cpf"><br>
+    RG <input type="text" required maxlength="11" name="rg"><br>
+    Endereco <input type="text" required maxlength="255" name="endereco"><br>
+    E-Mail <input type="email" required maxlength="255" name="email"><br>
+    Data de Nascimento <input type="date" required  name="nascimento"><br>
+    Número <input type="text" required maxlength="8" name="numero"><br>
+    Celular <input type="text" required maxlength="15" name="celular"><br>
+    Login <input type="text" required maxlength="64" name="login"><br>
+    Senha <input type="text" required maxlength="64" name="senha"><br>
+    <input type="submit" value="Atualizar Nutricionista">
+</form>
+
+<br>
+<a href="index.php">Voltar</a>
+
+<?php 
+
+rodape();
